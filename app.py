bf518a4f4bbb99fab72507d08a00e0b2ed393eb5
yozhgoor <yozhgoor@outlook.com>
2022-03-15 01:16:38 +0100

    Write the parsed dependencies in a file

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import argparse
+import os
+import shutil
 
 from utils import parse_dependency
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="pipenv-temp")
     parser.add_argument("dependencies", nargs='*', help="dependencies passed to pipenv")
+    parser.add_argument("--clear", default=False)
     args = parser.parse_args()
 
+    try:
+        pipfile = open("tmp/Pipfile", "w")
+    except FileNotFoundError:
+        os.mkdir("tmp")
+        pipfile = open("tmp/Pipfile", "w")
+
     for dependency in args.dependencies:
-        parsed = parse_dependency(dependency)
-        print(parsed.debug())
+        dependency = parse_dependency(dependency)
+        print(dependency.debug())
+
+        pipfile.write(dependency.debug())
+
+    pipfile = open("tmp/Pipfile", "r")
+    pipfile.close()
