88832bbdd76e232952bbe76af2679d961343dc9e
yozhgoor <yozhgoor@outlook.com>
2022-03-20 13:02:08 +0100

    Remove the `TestDependencyParsing` class

--- pipenvtemp.py
+++ pipenvtemp.py
@@ -74,64 +74,63 @@ def parse_dependency(dependency):
 
         return Dependency(dep_name, version)
 
-class TestDependencyParsing:
-    def test_dependency(self):
-        assert Dependency("requests", None) == parse_dependency("requests")
+def test_dependency():
+    assert Dependency("requests", None) == parse_dependency("requests")
 
-    def test_dependency_with_version(self):
-        assert Dependency("requests", "2.27") \
-            == parse_dependency("requests=2.27")
+def test_dependency_with_version():
+    assert Dependency("requests", "2.27") \
+        == parse_dependency("requests=2.27")
 
-    def test_dependency_with_exact_version(self):
-        assert Dependency("requests", "=2.27.1") \
-            == parse_dependency("requests==2.27.1")
+def test_dependency_with_exact_version():
+    assert Dependency("requests", "=2.27.1") \
+        == parse_dependency("requests==2.27.1")
 
-    def test_dependency_with_maximal_version(self):
-        assert Dependency("requests", "<2.25.1") \
-            == parse_dependency("requests=<2.25.1")
+def test_dependency_with_maximal_version():
+    assert Dependency("requests", "<2.25.1") \
+        == parse_dependency("requests=<2.25.1")
 
-    def test_repository_with_http_url(self):
-        assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib", None, None) \
-            == parse_dependency("matplotlib=https://github.com/matplotlib/matplotlib")
+def test_repository_with_http_url():
+    assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib", None, None) \
+        == parse_dependency("matplotlib=https://github.com/matplotlib/matplotlib")
 
-    def test_repository_with_http_url_and_no_extension(self):
-        assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib", None, None) \
-            == parse_dependency("https://github.com/matplotlib/matplotlib")
+def test_repository_with_http_url_and_no_extension():
+    assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib", None, None) \
+        == parse_dependency("https://github.com/matplotlib/matplotlib")
 
-    def test_repository_with_http_url_and_branch(self):
-        assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib.git", "old_master", None) \
+def test_repository_with_http_url_and_branch():
+    assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib.git", "old_master", None) \
             == parse_dependency("matplotlib=https://github.com/matplotlib/matplotlib.git#branch=old_master")
 
-    def test_repository_with_http_url_and_rev(self):
-        assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib.git", None, "f6e0ee4") \
-            == parse_dependency("matplotlib=https://github.com/matplotlib/matplotlib.git#rev=f6e0ee4")
+def test_repository_with_http_url_and_rev():
+    assert Repository("matplotlib", "https://github.com/matplotlib/matplotlib.git", None, "f6e0ee4") \
+        == parse_dependency("matplotlib=https://github.com/matplotlib/matplotlib.git#rev=f6e0ee4")
 
-    def test_repository_with_ssh_url(self):
-        assert Repository("pygame","ssh://git@github.com:pygame/pygame.git", None, None) \
-            == parse_dependency("pygame=ssh://git@github.com:pygame/pygame.git")
+def test_repository_with_ssh_url():
+    assert Repository("pygame","ssh://git@github.com:pygame/pygame.git", None, None) \
+        == parse_dependency("pygame=ssh://git@github.com:pygame/pygame.git")
 
-    def test_repository_with_ssh_url_and_no_extension(self):
-        assert Repository("pygame","ssh://git@github.com:pygame/pygame", None, None) \
-            == parse_dependency("ssh://git@github.com:pygame/pygame")
+def test_repository_with_ssh_url_and_no_extension():
+    assert Repository("pygame","ssh://git@github.com:pygame/pygame", None, None) \
+        == parse_dependency("ssh://git@github.com:pygame/pygame")
 
-    def test_repository_with_ssh_url_and_branch(self):
-        assert Repository("pygame","ssh://git@github.com:pygame/pygame.git", "android", None) \
-            == parse_dependency("pygame=ssh://git@github.com:pygame/pygame.git#branch=android")
+def test_repository_with_ssh_url_and_branch():
+    assert Repository("pygame","ssh://git@github.com:pygame/pygame.git", "android", None) \
+        == parse_dependency("pygame=ssh://git@github.com:pygame/pygame.git#branch=android")
 
-    def test_repository_with_ssh_url_and_rev(self):
-        assert Repository("pygame", "ssh://git@github.com:pygame/pygame.git", None, "c1afa68") \
-            == parse_dependency("pygame=ssh://git@github.com:pygame/pygame.git#rev=c1afa68")
+def test_repository_with_ssh_url_and_rev():
+    assert Repository("pygame", "ssh://git@github.com:pygame/pygame.git", None, "c1afa68") \
+        == parse_dependency("pygame=ssh://git@github.com:pygame/pygame.git#rev=c1afa68")
 
 if __name__ == '__main__':
     # Initiate the CLI
-    parser = argparse.ArgumentParser(prog="pipenv-temp")
+    parser = argparse.ArgumentParser(prog="pipenvtemp")
     parser.add_argument("dependencies", nargs='*', help="dependencies passed to pipenv")
 
     # Parse CLI args
     args = parser.parse_args()
 
     # Get the user cache directory
-    cache_dir = appdirs.user_cache_dir(appname='pipenv-temp')
+    cache_dir = appdirs.user_cache_dir(appname='pipenvtemp')
     if not os.path.isdir(cache_dir):
         os.mkdir(cache_dir)
 
